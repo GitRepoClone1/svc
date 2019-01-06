ec1f2fffbb29768c03d8faf133ae58bbc9143b95
Jawlit Patel <jawlitkp@gmail.com>
2019-01-06 17:50:12 -0500

    9th

--- install.sh
+++ install.sh
@@ -13,9 +13,5 @@ helm init --service-account tiller --wait
 echo "Waiting for tiller pods to become ready..."
 until helm version; do sleep 1; done
 
-echo "verify helm"
-# verify that helm is installed in the cluster
-kubectl get deploy,svc tiller-deploy -n kube-system
-
 # install jenkins chart
 helm install -f ./consul/values.yaml consul -n consul
