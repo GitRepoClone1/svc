64d6d272e8c902707f60ab366b54f4e14ec94a98
Jawlit Patel <jawlitkp@gmail.com>
2019-01-06 18:57:16 -0500

    9th

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gcloud auth activate-service-account --key-file devops.json
+gcloud auth activate-service-account --key-file ./consul-svc/devops.json
 
 gcloud container clusters get-credentials jenkins --zone us-east1-b --project devops-12232018
 
