979b33e6f30bf31d095a0ba167d542faefb9a5df
Jawlit Patel <jawlitkp@gmail.com>
2019-01-07 11:43:03 -0500

    debug

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+# download gcp json credential file
+gsutil cp gs://my-gcp-keys/devops.json ./consul-svc
+
+# activate servoice account
 gcloud auth activate-service-account --key-file ./consul-svc/devops.json
 
+# download gke credentials 
 gcloud container clusters get-credentials jenkins --zone us-east1-b --project devops-12232018
 
 # install jenkins chart
