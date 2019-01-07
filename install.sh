048626e9f6b68b5552ef008aa14453965a41ecbf
Jawlit Patel <jawlitkp@gmail.com>
2019-01-07 11:47:31 -0500

    debug

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ gsutil cp gs://my-gcp-keys/devops.json ./consul-svc
 gcloud auth activate-service-account --key-file ./consul-svc/devops.json
 
 # download gke credentials 
-gcloud container clusters get-credentials jenkins --zone us-east1-b --project devops-12232018
+gcloud container clusters get-credentials devops --zone us-east1-b --project devops-12232018
 
 # install jenkins chart
 helm install --name consul ./consul-svc/consul
