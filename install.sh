9f025f7284c96d9fab1eeab4200f3d5ff3d695c2
Jawlit Patel <jawlitkp@gmail.com>
2019-01-06 19:06:34 -0500

    9th

--- install.sh
+++ install.sh
@@ -4,17 +4,5 @@ gcloud auth activate-service-account --key-file ./consul-svc/devops.json
 
 gcloud container clusters get-credentials jenkins --zone us-east1-b --project devops-12232018
 
-kubectl --namespace kube-system create sa tiller
-# create a cluster role binding for tiller
-kubectl create clusterrolebinding tiller \
-    --clusterrole cluster-admin \
-    --serviceaccount=kube-system:tiller
-
-echo "initialize helm"
-# initialized helm within the tiller service account
-helm init --service-account tiller --wait
-
-sleep 10
-
 # install jenkins chart
-helm install -f ./consul/values.yaml consul -n consul
+helm install --name consul ./mychart -f ./consul/values.yaml
